a455ba187a212789ad77cc62607084df572b4a21
João Medrado <jcarloz@gmail.com>
2014-08-09 23:53:10 +1000

    Included a button

--- AlphaTouch/ViewController.m
+++ AlphaTouch/ViewController.m
@@ -17,12 +17,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CGRect viewRect = [[UIScreen mainScreen] bounds];
+  /*  CGRect viewRect = [[UIScreen mainScreen] bounds];
     UIView *colorView = [[UIView alloc] initWithFrame: viewRect];
     //Change the backgroundColor on the view to a UIColor with these values: red: 0.462, green: 0.749, blue: 0.937, and an alpha of 1.0, which should be this color:
     colorView.backgroundColor = [UIColor colorWithRed:0.462 green:0.749 blue:0.947 alpha:1.0];
     self.view = colorView;
 	// Do any additional setup after loading the view, typically from a nib.
+    */
+    self.view.backgroundColor = [UIColor yellowColor];
+    UIButton *firstButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    firstButton.frame = CGRectMake(120, 100, 80, 44);
+    [firstButton setTitle: @"Tap me!" forState:UIControlStateNormal];
+    [self.view addSubview:firstButton];
 }
 
 - (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
